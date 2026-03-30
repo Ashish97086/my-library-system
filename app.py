c97b9ab44479b5131caf2a4504b0028cb7fdc811
Ashish97086 <aak803110@gmail.com>
2026-03-30 17:50:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app.secret_key = "LMS_SUPER_SECRET_KEY"
 oauth = OAuth(app)
 google = oauth.register(
     name='google',
-    client_id=os.environ.get(GOOGLE_CLIENT_ID), 
-    client_secret=os.environ.get(GOOGLE_CLIENT_SECRET),
+    client_id=os.environ.get('GOOGLE_CLIENT_ID'), 
+    client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
